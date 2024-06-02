ad9803623c5773baf30dc3639d03a8769ec6b08a
Anshul Tamrakar <anshultamrakar487@gmail.com>
2024-06-02 17:06:38 +0530

    more changes

--- src/Practice_set7/ProductData.js
+++ src/Practice_set7/ProductData.js
@@ -6,6 +6,7 @@ import { fakeFetch } from './FakeFetch'
 const ProductData = () => {
     const [productData , setProductData] = useState([])
     const [sortData , setSortData] = useState([])
+    const [searchItem , setSearchItem] = useState("")
     const [isLoader , setIsLoader] = useState(true)
 
 
@@ -19,7 +20,7 @@ const ProductData = () => {
       const response = await fakeFetch("https://example.com/api/products")
       if(response.status === 200){
        setProductData(response.data.products)
-       setSortData(response?.data?.products.sort((a,b) => a.quantity - b.quantity))
+       setSortData(response?.data?.products.sort((a,b) => b.rating - a.rating))
        setIsLoader(false)
       }
       }catch(err){
@@ -27,12 +28,13 @@ const ProductData = () => {
       }
     }
 
-
+    const filterItems = 
 
   return (
     <div>
         <h2>Product Data</h2>
-        <ul>
+        <input type = "text" placeholder='Search product' value = {searchItem} onChange={(e) => setSearchItem(e.target.value)}/>
+        <ul> 
             {isLoader ? <p>Loading....</p> : sortData.map(product => (
                 <li>
                     <p>Name :{product.name}</p>
