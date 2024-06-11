d9ba691ac02f9de1b5fbb12d238bd56a08cce43e
Anshul Tamrakar <anshultamrakar487@gmail.com>
2024-06-11 23:25:24 +0530

    review order

---
+++ src/Tailwind/ReviewOrder.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ReviewOrder = () => {
+  return (
+    <div>
+        <div className='border-solid border-4 border-green-500  my-20 p-4'>
+          <h1 className='text-2xl'>Review Order</h1>
+          <button>Place order</button>
+        </div>
+    </div>
+  )
+}
+
+export default ReviewOrder
